36e77b7b89b74a529707eb72e705f78eab112495
Bismarck <bismarckdados@gmail.com>
2023-03-01 22:01:02 -0300

    feat: created title and subtitle in payment

---
+++ src/constants/colors.js
@@ -0,0 +1,3 @@
+const SUB_TITLE_COLOR = '#8E8E8E';
+
+export { SUB_TITLE_COLOR };
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,20 @@
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import { SUB_TITLE_COLOR } from '../../../constants/colors';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const StyledTypography = styled(Typography)`
+    margin-bottom: 20px !important;
+  `;
+  const StyledSubTitle = styled(Typography)`
+    color: ${SUB_TITLE_COLOR};
+    margin-bottom: 10px !important;
+  `;
+
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e Pagamento</StyledTypography>
+      <StyledSubTitle variant="h6">Ingresso escolhido</StyledSubTitle>
+    </>
+  );
 }
