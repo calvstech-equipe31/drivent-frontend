cad67669916830a7890b3b2a7d74bfd3ccbd1cbf
Bismarck <bismarckdados@gmail.com>
2023-03-05 20:51:12 -0300

    resolving merge.

--- src/components/Tickets/TicketTypes.js
+++ src/components/Tickets/TicketTypes.js
@@ -4,9 +4,12 @@ import { toast } from 'react-toastify';
 import styled from 'styled-components';
 
 export default function Tickets() {
-  const [tickets, setTickets] = useState([{ id: 1, name: 'online', price: '100', isRemote: true }, { id: 2, name: 'presencial', price: '250', isRemote: false }]);
+  const [tickets, setTickets] = useState([
+    { id: 1, name: 'online', price: '100', isRemote: true },
+    { id: 2, name: 'presencial', price: '250', isRemote: false },
+  ]);
   const [ticketSelected, setTicketSelected] = useState({});
-  const [type, setType] = useState(''); 
+  const [type, setType] = useState('');
   const [modality, setModality] = useState('');
   const [result, setResult] = useState(false);
   const [total, setTotal] = useState(0);
@@ -15,15 +18,15 @@ export default function Tickets() {
   const hotelPrice = 350;
 
   function selectTicketType(ticket) {
-    if(ticket.name===type) {
+    if (ticket.name === type) {
       return;
     }
     setTicketSelected(ticket);
-    if(ticket.isRemote) {
+    if (ticket.isRemote) {
       setResult(true);
       setTotal(ticket.price);
     }
-    if(!ticket.isRemote) {
+    if (!ticket.isRemote) {
       setModality('');
       setResult(false);
       setSum(ticket.price);
@@ -32,15 +35,15 @@ export default function Tickets() {
   }
 
   function selectModality(modality) {
-    if(modality==='hotel') {
+    if (modality === 'hotel') {
       setModality('hotel');
-      setTotal(Number(sum)+Number(hotelPrice));
+      setTotal(Number(sum) + Number(hotelPrice));
     }
-    if(modality==='noHotel') {
+    if (modality === 'noHotel') {
       setModality('noHotel');
       setTotal(sum);
     }
-    if(modality!=='') {
+    if (modality !== '') {
       setResult(true);
     }
   }
@@ -57,41 +60,57 @@ export default function Tickets() {
   return (
     <>
       <h1>Primeiro, escolha sua modalidade de ingresso</h1>
-      {tickets?.map((e) => <TicketStyle className={ ticketSelected === e ? 'selected' :'' } onClick={() => selectTicketType(e)}>{e.name}{e.price}</TicketStyle>)}
-      {type==='presencial'? 
-        ( 
-          <>
-            <h1>Ótimo! Agora escolha sua modalidade de hospedagem</h1>
-            <TicketStyle className={ modality === 'noHotel' ? 'selected' :'' } onClick={() => selectModality('noHotel')}>Sem hotel</TicketStyle>
-            <TicketStyle className={ modality === 'hotel' ? 'selected' :'' } onClick={() => selectModality('hotel')}>`Com hotel ${hotelPrice}` </TicketStyle>
-          </>
-        ) : ''
-      }
-      {result?<>`Fechado! O total ficou em ${total}. Agora é só confirmar`<Button onClick={() => sendTicket()}>Reservar ingresso</Button> </>:''}
+      {tickets?.map((e) => (
+        <TicketStyle className={ticketSelected === e ? 'selected' : ''} onClick={() => selectTicketType(e)}>
+          {e.name}
+          {e.price}
+        </TicketStyle>
+      ))}
+      {type === 'presencial' ? (
+        <>
+          <h1>Ótimo! Agora escolha sua modalidade de hospedagem</h1>
+          <TicketStyle className={modality === 'noHotel' ? 'selected' : ''} onClick={() => selectModality('noHotel')}>
+            Sem hotel
+          </TicketStyle>
+          <TicketStyle className={modality === 'hotel' ? 'selected' : ''} onClick={() => selectModality('hotel')}>
+            `Com hotel ${hotelPrice}`{' '}
+          </TicketStyle>
+        </>
+      ) : (
+        ''
+      )}
+      {result ? (
+        <>
+          `Fechado! O total ficou em ${total}. Agora é só confirmar`
+          <Button onClick={() => sendTicket()}>Reservar ingresso</Button>{' '}
+        </>
+      ) : (
+        ''
+      )}
     </>
   );
 }
 
-const TicketStyle= styled.div`
+const TicketStyle = styled.div`
   width: 100px;
   height: 100px;
-  border-radius:20px;
-  display:flex;
+  border-radius: 20px;
+  display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items:center;
+  align-items: center;
   &.selected {
     background-color: #ffeed2;
   }
 `;
 
-const Button= styled.div`
+const Button = styled.div`
   width: 180px;
   height: 40px;
-  display:flex;
+  display: flex;
   justify-content: center;
-  align-items:center;
-  border-radius:10px;
+  align-items: center;
+  border-radius: 10px;
   background-color: #ccc;
-  cursor:pointer;
+  cursor: pointer;
 `;
