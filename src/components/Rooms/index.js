cf8981d5966955c5e3d70be6a31b4a854c0d5e7f
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-03-10 16:50:53 -0300

    feat: rendering hotels

--- src/components/Rooms/index.js
+++ src/components/Rooms/index.js
@@ -6,16 +6,24 @@ import useHotelRooms from '../../hooks/api/useHotelRooms';
 import { toast } from 'react-toastify';
 import useSaveBooking from '../../hooks/api/useSaveBooking';
 import useBooking from '../../hooks/api/useBooking';
+import useHotels from '../../hooks/api/useHotels';
 
 export default function Rooms() {
   const [hotel, setHotel] = useState([]);
   const [roomAvailability, setRoomAvailability] = useState({});
   const [chosenRoom, setChosenRoom] = useState();
+  const [selectedHotel, setSelectedHotel] = useState(false);
   const { hotelRooms, hotelRoomsLoading, gethotelRooms } = useHotelRooms();
   const { booking, bookingLoading, bookingError } = useBooking();
   const { saveBooking, saveBookingLoading } = useSaveBooking();
+  const { hotels } = useHotels();
+
+  function selectHotel(hotelId) {
+    console.log(hotelId);
+  }
 
   function selectRoom(roomId) {
+    console.log(chosenRoom);
     const room = roomAvailability[roomId];
     console.log(roomAvailability);
     if (roomId === chosenRoom || !room.includes('empty')) return;
@@ -88,28 +96,101 @@ export default function Rooms() {
   console.log(roomAvailability);
   return (
     <>
-      <Tittle>Ótima pedida! Agora escolha seu quarto:</Tittle>
-      <AllRomns>
-        {hotelRoomsLoading
-          ? 'Aguardando...'
-          : hotel.Rooms.map((r) => (
-            <OneRoom
-              key={r.id}
-              selectRoom={() => selectRoom(r.id)}
-              chosenRoom={r.id === chosenRoom}
-              room={r}
-              roomAvailability={roomAvailability[r.id]}
-            />
-          ))}
-      </AllRomns>
-      <Button onClick={bookingRoom}>RESERVAR QUARTO</Button>
+      <MainTittle>
+        Escolha de hotel e quarto
+      </MainTittle>
+      { selectedHotel ? 
+        <>
+          <Tittle>Ótima pedida! Agora escolha seu quarto:</Tittle>
+          <AllRomns>
+            {hotelRoomsLoading
+              ? 'Aguardando...'
+              : hotel.Rooms.map((r) => (
+                <OneRoom
+                  key={r.id}
+                  selectRoom={() => selectRoom(r.id)}
+                  chosenRoom={r.id === chosenRoom}
+                  room={r}
+                  roomAvailability={roomAvailability[r.id]}
+                />
+              ))}
+          </AllRomns>
+          <Button onClick={bookingRoom}>RESERVAR QUARTO</Button>
+        </>
+        :
+        <>
+          <Tittle>
+            Primeiro, escolha seu hotel
+          </Tittle>
+          <HotelContainer>
+            {
+              hotels ?
+                hotels.map((hotel) => {
+                  return (
+                    <HotelBox onClick={() => selectHotel(hotel.id)}>
+                      <img src={hotel.image}/>
+                      <h1>{hotel.name}</h1>
+                      <h2>Tipos de acomodação:</h2>
+                      <h3>Bla, bla e bla</h3>
+                      <h2>Vagas disponíveis:</h2>
+                      <h3>xxx</h3>
+                    </HotelBox>
+                  );
+                })
+                :
+                <div></div>
+            }
+          </HotelContainer>
+        </>
+      }
     </>
   );
 }
 
+const HotelContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+const HotelBox = styled.div`
+  background-color: #EBEBEB;
+  width: 190px;
+  height: 230px;
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  border-radius: 10px;
+  margin: 10px 20px 0 0;
+  h1 {
+    color: #343434;
+    font-size: 20px;
+    font-weight: 400;
+    margin-top: 10px;
+  }
+  h2 {
+    color: #3C3C3C;
+    font-size: 12px;
+    font-weight: 700;
+    margin-top: 10px;
+  }
+  h3 {
+    color: #3C3C3C;
+    font-size: 12px;
+    font-weight: 400;
+  }
+`;
+
 const Tittle = styled.h1`
   color: #8e8e8e;
   font-size: 20px;
+`;
+
+const MainTittle = styled.div`
+  font-size: 30px;
+  font-weight: 400;
+  margin-bottom: 10px;
 `;
 
 const AllRomns = styled.div`
