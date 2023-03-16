e6ebb27dbfa5112c83a0bc2d464387e84542782e
nandasino <fernandamsino@gmail.com>
2023-03-16 17:59:31 -0300

    selecionar dias

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,36 @@
+import ListDays from '../../../components/Days';
+import styled from 'styled-components';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  return (
+    <>
+      <Container>
+        <MainTittle>Escolha de atividades</MainTittle>
+        <ListDays/>
+      </Container>
+    </>
+  );
 }
+const MainTittle = styled.div`
+  font-size: 30px;
+  font-weight: 400;
+  font-size: 34px;
+  line-height: 40px;
+  margin-bottom: 10px;
+`;
+const Container = styled.div`
+  padding: 30px;
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+
+  @media (max-width: 600px) {
+    height: calc(100vh - 80px);
+    padding: 20px;
+  }
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    
+    color: #000000;
+`;
---
+++ src/services/activitiesApi.js
@@ -0,0 +1,11 @@
+import api from './api';
+
+export async function getDays(token) {
+  const response = await api.get('/activities/', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
